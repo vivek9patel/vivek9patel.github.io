a627508ba98e761471963746ef6e452895d51447
Vivek Patel <vicky.p8980@gmail.com>
2021-03-23 14:34:48 +0530

    added theme-Yaru

--- src/components/util components/status.js
+++ src/components/util components/status.js
@@ -1,13 +1,31 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class Status extends Component {
-    render() {
-        return (
-            <div>
-                <span className="mx-2"><img src="./images/icons/status/network-wireless-signal-good-symbolic.svg" alt="ubuntu wifi" className="inline"/></span>
-                <span className="mx-2"><img src="./images/icons/status/audio-volume-medium-symbolic.svg" alt="ubuntu sound" className="inline"/></span>
-                <span className="mx-2"><img src="./images/icons/status/battery-good-symbolic.svg" alt="ubuntu battry" className="inline"/></span>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <span className="mx-2">
+          <img
+            src="./themes/Yaru/status/network-wireless-signal-good-symbolic.svg"
+            alt="ubuntu wifi"
+            className="inline"
+          />
+        </span>
+        <span className="mx-2">
+          <img
+            src="./themes/Yaru/status/audio-volume-medium-symbolic.svg"
+            alt="ubuntu sound"
+            className="inline"
+          />
+        </span>
+        <span className="mx-2">
+          <img
+            src="./themes/Yaru/status/battery-good-symbolic.svg"
+            alt="ubuntu battry"
+            className="inline"
+          />
+        </span>
+      </div>
+    );
+  }
 }
