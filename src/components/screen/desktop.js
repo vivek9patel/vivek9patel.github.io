43ce4ab601a3b5b820846819b02055a03f09c93e
Vivek Patel <vivek.p9737@gmail.com>
2021-04-10 16:39:19 +0530

    fixed responsiveness

--- src/components/screen/desktop.js
+++ src/components/screen/desktop.js
@@ -367,7 +367,7 @@ export class Desktop extends Component {
         }
 
         return (
-            <div className="absolute rounded-md top-1/2 left-1/2 text-center text-white font-light text-sm bg-ub-cool-grey transform -translate-y-1/2 -translate-x-1/2 w-96">
+            <div className="absolute rounded-md top-1/2 left-1/2 text-center text-white font-light text-sm bg-ub-cool-grey transform -translate-y-1/2 -translate-x-1/2 sm:w-96 w-3/4 z-50">
                 <div className="w-full flex flex-col justify-around items-start pl-6 pb-8 pt-6">
                     <span>New folder name</span>
                     <input className="outline-none mt-5 px-1 w-10/12  context-menu-bg border-2 border-yellow-700 rounded py-0.5" id="folder-name-input" type="text" autoComplete="off" spellCheck="false" autoFocus={true} />
