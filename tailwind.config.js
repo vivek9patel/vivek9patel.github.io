1c66c572bbeb4fe1f653bc8547b1949ef4602259
vivek9patel <vivek.p9737@gmail.com>
2021-06-15 21:31:21 +0530

    adds craco build

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  purge: [],
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     backgroundColor: theme => ({
