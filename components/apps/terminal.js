2dbd5abf939e35f9a3ca36b760d0827031078888
cloudsofmana <32783555+cloudsofmana@users.noreply.github.com>
2022-06-17 21:06:48 -0500

    T

--- components/apps/terminal.js
+++ components/apps/terminal.js
@@ -14,11 +14,11 @@ export class Terminal extends Component {
         this.child_directories = {
             root: ["books", "projects", "personal-documents", "skills", "languages", "PDPU", "interests"],
             PDPU: ["Sem-6"],
-            books: ["Eric-Jorgenson_The-Almanack-of-Naval-Ravikant.pdf", "Elon Musk: How the Billionaire CEO of SpaceX.pdf", "The $100 Startup_CHRIS_GUILLEBEAU.pdf", "The_Magic_of_Thinking_Big.pdf"],
-            skills: ["Front-end development", "React.js", "jQuery", "Flutter", "Express.js", "SQL", "Firebase"],
-            projects: ["vivek9patel-personal-portfolio", "synonyms-list-react", "economist.com-unlocked", "Improve-Codeforces", "flutter-banking-app", "Meditech-Healthcare", "CPU-Scheduling-APP-React-Native"],
-            interests: ["Software Engineering", "Deep Learning", "Computer Vision"],
-            languages: ["Javascript", "C++", "Java", "Dart"],
+            books: ["How to Unfuck Yourself", "Hitchhiker's Guide To The Galaxy"],
+            skills: ["Front-end development", "System Administration", "Networking", "Linux", "Coding", "Security", "Server Administration"],
+            projects: ["CloudsOfMana-personal-portfolio", "Discord Bots", "ZeroTier", "UPNP Virtual Lan", "Heroku and ZEET app deployments", "Discord Administrator", "Botnet"],
+            interests: ["Software Engineering", "Deep Learning", "Quantum Computing"],
+            languages: ["Javascript", "C++", "Java", "html5"],
         };
         this.state = {
             terminal: [],
@@ -56,7 +56,7 @@ export class Terminal extends Component {
             <React.Fragment key={id}>
                 <div className="flex w-full h-5">
                     <div className="flex">
-                        <div className=" text-ubt-green">vivek@Dell</div>
+                        <div className=" text-ubt-green">clouds@mana</div>
                         <div className="text-white mx-px font-medium">:</div>
                         <div className=" text-ubt-blue">{this.current_directory}</div>
                         <div className="text-white mx-px font-medium mr-1">$</div>
