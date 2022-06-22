183c1018c7f17bf6d158ed3eb4aedeb7e0f926d8
cloudsofmana <32783555+cloudsofmana@users.noreply.github.com>
2022-06-22 13:30:24 -0500

    Update apps.config.js

--- apps.config.js
+++ apps.config.js
@@ -5,7 +5,7 @@ import { displaySettings } from './components/apps/settings';
 import { displayChrome } from './components/apps/chrome';
 import { displayTrash } from './components/apps/trash';
 import { displayGedit } from './components/apps/gedit';
-import { displayAboutClouds } from './components/apps/clouds';
+import { displayAboutClouds } from './components/apps/vivek';
 import { displayTerminalCalc } from './components/apps/calc';
 
 const apps = [
