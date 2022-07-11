2013a905ed5830a05804b721255b83d9af459fc6
cloudsofmana <32783555+cloudsofmana@users.noreply.github.com>
2022-07-10 21:34:29 -0500

    Spotify

--- components/apps/spotify.js
+++ components/apps/spotify.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Spotify() {
     return (
-        <iframe src="https://open.spotify.com/embed/playlist/37i9dQZEVXbLZ52XmnySJg" frameBorder="0" title="Spotify" className="h-full w-full bg-ub-cool-grey"></iframe>
+        <iframe src="https://open.spotify.com/embed/playlist/5DIB57G60gaCwA5NKFaJuX?si=ndmrlPeGToy2Lk5JxQE8bA&utm_source=copy-link " frameBorder="0" title="Spotify" className="h-full w-full bg-ub-cool-grey"></iframe>
     )
 }
 
