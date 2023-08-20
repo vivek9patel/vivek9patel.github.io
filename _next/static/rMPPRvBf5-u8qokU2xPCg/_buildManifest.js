c0c00083d9d4e44e0480b524735fad730f79ac67
vivek9patel <vivek.p9737@gmail.com>
2023-08-20 18:15:32 +0000

    Deploying to gh-pages from @ vivek9patel/vivek9patel.github.io@c2bb09e081549f1d245abb608b7649fcf49939d8 🚀

---
+++ _next/static/rMPPRvBf5-u8qokU2xPCg/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/b637e9a5-6cf11adc33c7fdd4.js","static/chunks/638-9e42104b80766292.js","static/chunks/pages/index-3052149d7ed3a49b.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
