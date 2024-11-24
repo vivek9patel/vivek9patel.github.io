c251b6246d00198b31fcb598b068bce2293ef316
vivek9patel <vivek.p9737@gmail.com>
2024-11-24 07:10:42 +0000

    Deploying to gh-pages from @ vivek9patel/vivek9patel.github.io@c4f1eff15aaba964fb0ad807afb4e233c33364dd 🚀

--- _next/static/3_eJ-y4kovLxoIDLO0UKq/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/b637e9a5-6cf11adc33c7fdd4.js","static/chunks/269-d3f19ef322895985.js","static/chunks/pages/index-79d56e747f76610b.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/xW5Q4vaP0fZOk9eKniVXl/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/b637e9a5-6cf11adc33c7fdd4.js","static/chunks/269-d3f19ef322895985.js","static/chunks/pages/index-57388d32335ecd17.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
